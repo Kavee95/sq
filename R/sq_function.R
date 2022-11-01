3de5e4253a80d7ba965b6517583bb1f74831e8c8
Kavee95 <kkavindi.fernando@gmail.com>
2022-11-01 12:01:59 -0700

    update

--- R/sq_function.R
+++ R/sq_function.R
@@ -3,8 +3,13 @@
 #' @param x any number
 #' @keywords squares
 #' @export
+#' @import graphics
+#' @importFrom graphics
 #' @examples
 #' squares()
+
 squares <- function(x) {
-  print(paste0("The square of ", x, " is ", round((x^2), 4)))
+print(paste0("The square of ", x, " is ", round((x^2), 4)))
+install.packages("graphics")
+graphics::plot(x = x, y = x^2)
 }
